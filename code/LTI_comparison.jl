9bae258f77e9dfa54a35eda4cc65d6702628781e
Shen Shen <shenshen@mit.edu>
2017-05-04 17:06:50 -0400

    minor update

--- code/LTI_comparison.jl
+++ code/LTI_comparison.jl
@@ -1,14 +1,24 @@
 using JuMP
 using Mosek
 
+
+
+# generate candiates that admit blkdiag form 
+# solutions
 function generate_candidate_A(n,num_of_instances)
     Q=eye(n)
     A=randn(n,n)
 
 end
 
+# solves the blkdiag form optimization
+# if feasible, returns value, timeit, and stores A
 
+function blkdiag_optimization(A,blk_size)
+end
 
+# with the scaling facotors fixed, sovles the low-dimensial optimization, and
+# returns the time, the solutions
 function LTI_test(partitions=4)
     A = randn(60,60)
     tic()
@@ -31,3 +41,4 @@ end
 
 # LTI_test()
 # composed_test()
+# adding plotting functionality
