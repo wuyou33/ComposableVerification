463b0e66b3ad16ff558964bc94013c9398d0177f
Shen Shen <shenshen@mit.edu>
2017-05-16 12:16:02 -0400

    for sync

--- code/LTI_comparison.py
+++ code/LTI_comparison.py
@@ -42,66 +42,53 @@ def lumped(n,blk_size,feasibility_only=True, quite=True):
             lumped_time = 'inf'    
         return A, lumped_time, P
 
-
-# lumped(6,4)
-
 # form n codependent LMIs of the origianl problem size to search for good scaling factors
 def direct_scaling(A, n, blk_size, feasibility_only=True, quite=True):
     blks=n/blk_size
     P=[]
     N=[]
+    PAAP=[]
     with Model("direct_scaling") as M:    
         # Setting up the variables
         for i in range(blks):
             P.append(M.variable(Domain.inPSDCone(blk_size)))
             N.append(M.variable(Domain.inPSDCone(blk_size)))
+        # for i in range(blks):
             # extracting the corresponding A submatrices
-            row_slice = Expr.mul(P[i],A[(i-1)*blk_size:i*blk_size,:])
-            collum_slice = row_slice.transpose()
-            for j in range(blks):
-               
-                # row_stacking = 
-                # Aii=A[]
-                # setting up the constraints, essentially solving for n independent LMIs
-                M.constraint(Expr.neg(Expr.add(Expr.mul(P,A),Expr.mul(A.transpose(),P))),Domain.inPSDCone())
+            # print(P[i])
+            row_slice = Expr.mul(P[i],A[(i)*blk_size:(i+1)*blk_size,:])
 
-def testing_stuff():
-    A=np.eye(12)
-    blk_size=3
-    i=3
-    print(A[(i-1)*blk_size:i*blk_size,:])
-    # with Model("testing") as M:    
-        # Setting up the variables
-        # P = M.variable("P", Domain.inPSDCone(10))
-        # print(P.pick([3,4,5],[3,4,5]))
-        # print(P.pick([i],[j]))
-        # structure constraint
-        # for i in range(n):
-        #     for j in range(n):
-        #             if j >=((i/blk_size)+1)*blk_size or j< (i/blk_size)*blk_size:
-        #                 M.constraint(P.pick([i],[j]), Domain.equalsTo(0.0))
+            colum_slice = row_slice.transpose()
+            # print(row_slice.size())
+            if i == 0:
+                rowstaced=Expr.vstack(row_slice,Expr.reshape(Expr.zeros((blks-1)*blk_size*n),(blks-1)*blk_size,n))
+                columnstaced=Expr.hstack(colum_slice,Expr.reshape(Expr.zeros((blks-1)*blk_size*n),n,(blks-1)*blk_size))
+            elif i==n:
+                rowstaced=Expr.vstack(Expr.reshape(Expr.zeros((blks-1)*blk_size*n),(blks-1)*blk_size,n),row_slice)
+                columnstaced=Expr.hstack(Expr.reshape(Expr.zeros((blks-1)*blk_size*n),n,(blks-1)*blk_size),colum_slice)
+            else:
+                ontop=Expr.reshape(Expr.zeros((i)*blk_size*n),(i)*blk_size,n)
+                onbuttom=Expr.reshape(Expr.zeros((blks-i-1)*blk_size*n),(blks-i-1)*blk_size,n)
+                rowstaced=Expr.vstack(ontop,row_slice,onbuttom)
 
-        # # lyapunov constraints
-        # M.constraint(Expr.neg(Expr.add(Expr.mul(P,A),Expr.mul(A.transpose(),P))),Domain.inPSDCone())
+                onLeft=Expr.reshape(Expr.zeros((i)*blk_size*n),n,(i)*blk_size)
+                onRight=Expr.reshape(Expr.zeros((blks-i-1)*blk_size*n),n,(blks-i-1)*blk_size)
+                columnstaced=Expr.hstack(onLeft,colum_slice,onRight)
+            PAAP.append(Expr.add(rowstaced,columnstaced))
+        # print(PAAP[0])
+        # setting up the constraints, essentially solving for n independent LMIs
+        # M.constraint(Expr.neg(member_LMI),Domain.inPSDCone())
+direct_scaling(np.random.randn(6,6),6,2)
+
+def construct_blk_diag(N, blk_size):
+    zero_blks = Expr.reshape(Expr.zeros(blk_size*blk_size),blk_size,blk_size)
+
+    for i in range(len(N)):
+        Expr.hstack(N[i],zero_blks)
 
 
-        # if not feasibility_only:
-        #     M.objective(ObjectiveSense.Minimize, Expr.dot(P,Q))
 
-        # if not quite:
-        #     M.setLogHandler(sys.stdout)
-
-        # M.solve()
-        # status = M.getSolverDoubleInfo("intpntOptStatus")
-        # # print("solver status " + str(status))
-        # if status == 1:
-        #     lumped_time = M.getSolverDoubleInfo("intpntTime")
-
-        #     # print("solver lumped_time " + str(lumped_time))
-        #     # print(P.level())
-        # else:
-        #     lumped_time = 'inf'    
-        # return A, lumped_time, P
+    return blk_diag_form 
 
 # w/ fixed scaling factor 
 def fixed_scaling(A,blk_size):
@@ -124,4 +111,4 @@ def write_data(key, value):
 
 
 
-testing_stuff()
+# testing_stuff()
--- code/tes.py
+++ code/tes.py
@@ -1,8 +1,36 @@
-import cvxpy as cvx
+import sys
+import mosek
+from   mosek.fusion import *
 import numpy as np
-W = cvx.Variable(3,3)
-S= cvx.Variable(3,3)
-B = np.zeros([3,6])
-# T = cvx.diag(cvx.vstack(B, W))
-T=cvx.hstack(W,S,B)
-print(T)
+
+A=np.eye(10)
+blk_size=5
+i=2
+
+with Model("direct_scaling") as M: 
+	# A=Matrix.sparse(10,10,) 
+	P1=M.variable(Domain.inPSDCone(6))
+
+
+	# zero_block=Expr.reshape(Expr.zeros(36))
+	zero_block=Expr.zeros(1)
+	slices=Expr.ones(1)
+
+	print(slices)
+	print(Expr.hstack(zero_block,slices))
+# 
+	# M.constraint(zero_block.slice(4,5),Domain.equalsTo(slices))
+# 
+	# P.reshape(6,6)
+	# A[0:5,0:5]=P
+	# PP=Var.reshape(P1,36,1)
+
+	# P=Var.vstack(PP,Var.hstack(zero_block,P2))
+
+	# # A = Matrix.eye(6)
+	# M.constraint(Expr.neg(Expr.add(Expr.mul(P,A),Expr.mul(A.transpose(),P))),Domain.inPSDCone())
+	# M.solve()
+
+print(zero_block)
+
+# print(A[(i-1)*blk_size:i*blk_size,:])
